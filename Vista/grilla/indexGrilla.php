1a8f086c3dcdc301d236932cf5d25ab7fc6da033
frankitobienslow <franinsua7@gmail.com>
2023-11-14 11:06:28 -0300

    precios

--- Vista/grilla/indexGrilla.php
+++ Vista/grilla/indexGrilla.php
@@ -6,12 +6,6 @@ include_once '../estructura/headPrivado.php';
 $objProducto = new AbmProducto();
 $listaProductos = $objProducto->buscar(null);
 
-// generacion aleatoria de precios
-$precio = array();
-for ($i = 0; $i < count($listaProductos); $i++) {
-  $precio[$i] = rand(1000, 100000);
-} // fin for 
-$count = 0;
 // RECORIDO DE LOS PRODUCTOS CON SU IMAGEN, NOMBRE Y PRECIO
 ?>
 <div class="d-flex flex-wrap pb-5 justify-content-center">
@@ -25,7 +19,7 @@ $count = 0;
       <div class="card-body d-flex flex-column justify-content-end" style="max-height:200px">
         <h5 class="card-title" id="nombreProducto"><?php echo ($unProducto->getNombre()) ?></h5>
         <p class="card-text" id="detalle"> <?php echo ($unProducto->getDetalle()) ?> </p>
-        <p class="card-text" id="precio"> <?php echo ("$" . $precio[$count]) ?> </p>
+        <p class="card-text" id="precio"> <?php echo ("$" . $unProducto->getPrecio()) ?> </p>
         <a href="#" class="btn btn-primary" id="carrito">Añadir Carrito</a>
       </div>
     </div>
