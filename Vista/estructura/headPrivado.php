da49c366b3076a0d8898e6a5147075db331c34de
frankitobienslow <franinsua7@gmail.com>
2023-11-14 10:47:21 -0300

    redireccion a inicio desde nav

--- Vista/estructura/headPrivado.php
+++ Vista/estructura/headPrivado.php
@@ -47,7 +47,7 @@
 <body>
   <nav class="navbar navbar-expand-lg bg-light p-2 fs-3">
     <div class="container-fluid">
-      <a class="navbar-brand" id="pagina-principal" href="../estructura/principal.php">Grupo N°5</a>
+      <a class="navbar-brand" id="pagina-principal" href="../inicio/inicioIndex.php">Grupo N°5</a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
